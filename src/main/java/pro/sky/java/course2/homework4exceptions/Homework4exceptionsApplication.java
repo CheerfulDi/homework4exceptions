fff1a643717d2ea6e3879b08f627caf0b2098e11
Диана <diana_yudina@mail.ru>
2021-12-05 15:36:50 +0500

    Initial commit

---
+++ src/main/java/pro/sky/java/course2/homework4exceptions/Homework4exceptionsApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course2.homework4exceptions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Homework4exceptionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Homework4exceptionsApplication.class, args);
+    }
+
+}
